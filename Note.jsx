a6ccb551859971e50fd2e49d1de3f15a698fc811
GOPISWARA RAO CHINNI <66155898+GOPISWARA@users.noreply.github.com>
2021-06-07 14:19:37 +0530

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note() {
   return (
-    <div classname="note">
+    <div className="note">
       <h1>Javascript and React.js</h1>
       <p>
         This was an amazing bootcamp taken up by Shaurya Sinha .We covered
